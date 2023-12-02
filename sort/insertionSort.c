8411679cc8bfe0cdf37ec2ce3121297a6659550c
21rr10 <ranjan.ritesh21102003@gmail.com>
2023-12-03 01:10:19 +0530

    insertion sort

---
+++ sort/insertionSort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define MAX 100
+static int len;
+
+void insertionSort(int a[]){
+            int i;
+            
+            // Sort the array using insertion sort
+            for (i = 0; i < len; i++){
+                 int curr=a[i];
+                 int prev=i-1;
+                 while(prev>=0 && a[prev]>curr){
+                    a[prev+1]=a[prev];
+                    prev--;
+                 }
+                 a[prev+1]=curr;
+                }
+            }
+
+
+void print(int a[]){
+    int i;
+    printf("\nSorted numbers in ascending order:\n");
+            for (i = 0; i < len; i++)
+            {
+                printf("%d ", a[i]);
+                printf("\n");
+            }
+}
+
+int main()
+        {
+            int a[MAX];
+            int i, j;
+            printf("enter the size of the array");
+            scanf("%d",&len);
+            printf("Enter %d numbers:",len);
+            for (i = 0; i < len; i++)
+            {
+                scanf("%d", &a[i]);
+            }
+            insertionSort(a);
+            print(a);
+        
+
+            return 0;
+        }
